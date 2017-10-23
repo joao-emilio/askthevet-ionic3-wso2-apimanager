3a8aad3b189a98779d9c91dcaac2d632a4091b04
Joao Emilio <joaoemilio@devthinkers.com>
2017-10-23 20:08:16 -0200

    enviar perguntas

--- src/pages/ask-question/ask-question.ts
+++ src/pages/ask-question/ask-question.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {RequestOptions, Http, Headers} from "@angular/http";
 import {Ask} from "../../models/Ask";
+import { ToastController } from "ionic-angular";
 
 /**
  * Generated class for the AskQuestionPage page.
@@ -17,11 +18,24 @@ import {Ask} from "../../models/Ask";
 })
 export class AskQuestionPage {
   item: any;
-  ask: Ask;
+  ask = new Ask();
+  perguntas: Ask[];
 
-  constructor(public navCtrl: NavController, navParams: NavParams, public http: Http ) {
+  constructor(public navCtrl: NavController, navParams: NavParams, public http: Http,
+              public toastCtrl: ToastController ) {
     this.item = navParams.get('item');
     console.log( this.item.firstName );
+    let api_url = "https://gateway.api.cloud.wso2.com:443/t/wso2apiforum/vets/v1.0.0/askthevet/" + this.item.id;
+
+    let self = this;
+
+    let headers = new Headers();
+    headers.append('Authorization', 'Bearer 2727584b-979f-3a4e-8b50-145e7f6c6ed2');
+    let options = new RequestOptions( { headers: headers });
+    this.http.get( api_url, options ).subscribe( data => {
+      console.log( "get perguntas retornou: " + data );
+      self.perguntas = data.json() as Ask[];
+    });
   }
 
 
@@ -30,15 +44,23 @@ export class AskQuestionPage {
   }
 
   done() {
-    this.ask = new Ask();
-    this.ask.question = "teste";    let api_url = "https://gateway.api.cloud.wso2.com:443/t/wso2apiforum/vets/v1.0.0/askthevet";
+    //this.ask.question = "teste";
+    this.ask.vetId = this.item.id;
+    let api_url = "https://gateway.api.cloud.wso2.com:443/t/wso2apiforum/vets/v1.0.0/askthevet";
+
+    let self = this;
 
     let headers = new Headers();
     headers.append('Authorization', 'Bearer 2727584b-979f-3a4e-8b50-145e7f6c6ed2');
     let options = new RequestOptions( { headers: headers });
     this.http.post( api_url, this.ask, options ).subscribe( data => {
       console.log( "post retornou: " + data );
+      let toast = this.toastCtrl.create( { message: 'Sua pergunta foi enviada.', duration: 3000 });
+      toast.present();
+      self.ask = new Ask();
+      self.ask.question = "";
     });
+
 
   }
 
